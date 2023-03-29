4386d2cc40ff4da029acf3ad80a3e320f8f7423e
Parkreiner <throwawayclover@gmail.com>
2023-03-29 12:46:17 -0400

    wip: Make slighly better temp version of component

--- src/components/CharacterPreview/CharacterPreview.tsx
+++ src/components/CharacterPreview/CharacterPreview.tsx
@@ -1,5 +1,27 @@
-export default function CharacterPreview() {
+import { Character } from "@/typesConstants/gameData";
+import { CharacterColors } from "@/typesConstants/colors";
+
+type Props = {
+  selectedCharacter: Character | null;
+  colors: CharacterColors;
+};
+
+export default function CharacterPreview({ selectedCharacter, colors }: Props) {
+  const characterClass = selectedCharacter?.class ?? "Unknown";
+  const id = selectedCharacter?.id ?? "Unknown";
+
   return (
-    <div className="min-w-[500px] flex-grow self-stretch bg-white">Hi :)</div>
+    <div className="min-w-[500px] flex-grow self-stretch bg-white">
+      <p>
+        Class: {characterClass}
+        <br />
+        ID: {id}
+      </p>
+
+      <div
+        className="h-[200px] w-[200px]"
+        style={{ backgroundColor: colors.skin[0] }}
+      />
+    </div>
   );
 }
