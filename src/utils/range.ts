e2d2f814900857242d2858f0cf9996bdbb7eb618
Parkreiner <throwawayclover@gmail.com>
2023-03-29 11:22:30 -0400

    feat: Add range util

---
+++ src/utils/range.ts
@@ -0,0 +1,19 @@
+/**
+ * Produces an array representing an array of numbers. Range is inclusive on the
+ * start bound, but exclusive on the end bound.
+ *
+ * If both bounds are provided, bound1 will be treated as the start, and bound2
+ * will be the end. If only bound1 is provided, it will be treated as the end,
+ * and the start bound will become 0.
+ */
+export function range(bound1: number, bound2?: number): number[] {
+  const startBound = bound2 === undefined ? 0 : bound1;
+  const endBound = bound2 ?? bound1;
+
+  const output: number[] = [];
+  for (let i = startBound; i < endBound; i++) {
+    output.push(i);
+  }
+
+  return output;
+}
