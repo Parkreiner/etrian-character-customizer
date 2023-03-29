ce5a677c123d2b938ba47fcb426d9431e7734e1d
Parkreiner <throwawayclover@gmail.com>
2023-03-29 11:30:51 -0400

    feat: Add basic version of LoadingIndicator

---
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,39 @@
+/**
+ * @file A basic loading indicator for the top-level app.
+ */
+import { useEffect, useState } from "react";
+import { range } from "@/utils/range";
+
+const maxDots = 3;
+const dotRange = range(1, maxDots + 1);
+
+export default function LoadingIndicator() {
+  const [visibleDots, setVisibleDots] = useState(0);
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setVisibleDots((currentDots) => (currentDots + 1) % (maxDots + 1));
+    }, 500);
+
+    return () => window.clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center text-lg text-neutral-900">
+      <div className="mb-4 h-[200px] w-[200px] rounded-full bg-teal-700">
+        {/**
+         * @todo Add tree-like SVG here once I make one.
+         */}
+      </div>
+
+      <p className="pl-4">
+        Loading
+        {dotRange.map((dotNum) => (
+          <span key={dotNum} style={{ opacity: dotNum <= visibleDots ? 1 : 0 }}>
+            .
+          </span>
+        ))}
+      </p>
+    </div>
+  );
+}
