40358392101ad1adb775f8415bdf31f68144d68c
Parkreiner <throwawayclover@gmail.com>
2023-03-28 20:44:51 -0400

    feat: Add basic number input for HueWheel

--- src/components/ColorPicker/HueWheel.tsx
+++ src/components/ColorPicker/HueWheel.tsx
@@ -28,24 +28,29 @@ export default function HueWheel({ hue, onHueChange }: Props) {
   }, [hue]);
 
   return (
-    <div ref={containerRef} style={{ position: "relative" }}>
-      <div>
-        <label htmlFor={textId}>Hue</label>
-        {/* Ranges aren't 0-360 to make it easy to do hue wrap-arounds */}
-        <input
-          id={textId}
-          type="number"
-          min="-1"
-          max="361"
-          step="1"
-          value={hue}
-          onChange={(e) => {
-            const eventHue = e.target.valueAsNumber;
-            const dispatchedHue = eventHue < 0 ? 360 : eventHue % 361;
-            onHueChange(dispatchedHue);
-          }}
-        />
-        °
+    <div ref={containerRef} className="relative">
+      <div className="flex flex-col items-center text-yellow-50">
+        <label htmlFor={textId} className="font-bold">
+          Hue
+        </label>
+        <span>
+          {/* Ranges are a little funky to make hue wrap-arounds easier */}
+          <input
+            id={textId}
+            className="no-arrow h-16 bg-teal-600 text-right text-[72px] font-extrabold hover:ring-red-900"
+            type="number"
+            min="-1"
+            max="360"
+            step="1"
+            value={hue}
+            onChange={(e) => {
+              const eventHue = e.target.valueAsNumber;
+              const dispatchedHue = eventHue < 0 ? 359 : eventHue % 360;
+              onHueChange(dispatchedHue);
+            }}
+          />
+          <span className="text-[72px]">°</span>
+        </span>
       </div>
 
       <div ref={sliderRef} style={{ position: "absolute" }}></div>
