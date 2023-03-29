0890697e07e7e6ed4a9ed6eefe6d27e6efc3bfca
Parkreiner <throwawayclover@gmail.com>
2023-03-28 20:46:02 -0400

    fix: Remove issue where HSV state could get out of sync with props

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,100 +1,103 @@
 /**
- * @file A component that lets you pick colors via RGB or HSB. Chose to use HSB
- * instead of HSL because it's more intuitive to someone who's used a lot of
- * Adobe products.
+ * @file A component that lets you pick colors via RGB or HSV.
  *
- * @todo Fix and validate all the color conversion helper functions (RGB and hex
- *       should be fine; the HSB conversions are the big concern)
- * @todo Implement the useStableHsb hook for a better user experience
- * @todo Figure out why the Radix sliders aren't appearing in the
- *       SatBrightnessInput component (the sliders might be there; they might
- *       just need a ton of CSS?)
+ * Chose to use HSV instead of HSL because it's more intuitive to someone who's
+ * used a lot of Adobe products (even though only HSL is supported by CSS).
+ *
+ * The rest of the app is using hex codes to make working with colors easier.
+ * This is the only top-level component that needs to be aware of the RGB and
+ * HSV formats. However, RGB->HSV and HSV->RGB are both lossy formulas, and the
+ * numbers can sometimes do weird things, which, when you have controlled inputs
+ * bound to the numbers, can make them also do weird things. The HSV values had
+ * to be separated into state to keep the UI feeling nice and not janky.
+ *
+ * Notable UI jank caused by the formulas:
+ * - Anytime saturation goes to 0, hue is also supposed to be 0. But having hue
+ *   change because you were changing a different channel felt weird. Hue would
+ *   also be forever stuck at 0 in the UI until you added saturation back.
+ * - Because HSV->RGB/Hex is lossy, there would be cases (especially for the
+ *   brighter colors) where you would change something in the HSV inputs, which
+ *   would make a new HSV object. The HSV would then be converted to hex, and
+ *   the hex would be passed to onHexChange. But the new hex string would be the
+ *   same as the old hex string, and so nothing would re-render. The only way to
+ *   get the UI unstuck was by changing a V value or an RGB value.
+ *
  * @todo Figure out how to implement the HueWheel component and make sure that
- *       the movements are locked to a circle
+ * the movements are locked to a circle (this will probably be hard)
  */
-import { useState, useRef } from "react";
-import SaturationBrightnessInputs from "./SatBrightnessInputs";
-import { HSBColor, RGBColor } from "./localTypes";
-import { hexToRgb, rgbToHsb, hsbToHex, rgbToHex } from "./colorHelpers";
+import { useState } from "react";
+import { HSVColor, RGBColor } from "./localTypes";
+import { hexToRgb, rgbToHsv, hsvToHex, rgbToHex } from "./colorHelpers";
+
+import ValueSaturationInputs from "./ValueSaturationInputs";
 import RgbInputs from "./RgbInputs";
 import HueWheel from "./HueWheel";
 
 type Props = {
   hexColor: string;
-  onColorChange: (newHexColor: string) => void;
+  onHexChange: (newHexColor: string) => void;
 };
 
-/**
- * Lot of things to consider/test out/research for this hook.
- *
- * @todo Fix the base rgbToHsb function so that the hue shift from 299 to 300
- *       doesn't break things. It should go 299 to 300, not 299 to -60
- * @todo Implement the hook so that going from a saturation of 1 to 0 doesn't
- *       affect the hue. The hue should go to 0 in traditional HSB calculations,
- *       but it's not the best user experience.
- * @todo Figure out the best way to prevent one HSB channel from jumping around
- *       as the result of another HSB channel changing (rounding issues?)
- * @todo Research whether it makes sense to turn this into a useStableColors
- *       hook that
- */
-function useStableHsb(rgb: RGBColor): HSBColor {
-  return rgbToHsb(rgb);
-}
-
-function ColorPicker({ hexColor, onColorChange }: Props) {
+export default function ColorPicker({ hexColor, onHexChange }: Props) {
   const rgb = hexToRgb(hexColor);
-  const hsb = useStableHsb(rgb);
+  const [hsv, setHsv] = useState(() => rgbToHsv(rgb));
+  const [cachedHex, setCachedHex] = useState(hexColor);
 
   const onRgbChannelChange = (channel: keyof RGBColor, newValue: number) => {
     const inputInvalid =
       !Number.isInteger(newValue) || newValue < 0 || newValue > 255;
 
-    if (inputInvalid) return;
-    const newHex = rgbToHex({ ...rgb, [channel]: newValue });
-    onColorChange(newHex);
+    if (inputInvalid) {
+      return;
+    }
+
+    const newRgb = { ...rgb, [channel]: newValue };
+    const newHex = rgbToHex(newRgb);
+
+    setHsv(rgbToHsv(newRgb));
+    setCachedHex(newHex);
+    onHexChange(newHex);
   };
 
-  const onHsbChannelChange = (channel: keyof HSBColor, newValue: number) => {
+  const onHsvChannelChange = (channel: keyof HSVColor, newValue: number) => {
     const inputInvalid =
       !Number.isInteger(newValue) ||
       newValue < 0 ||
-      (channel === "hue" && newValue > 360) ||
-      ((channel === "bri" || channel === "sat") && newValue > 100);
+      (channel === "hue" && newValue > 359) ||
+      ((channel === "val" || channel === "sat") && newValue > 100);
 
-    if (inputInvalid) return;
-    const newHex = hsbToHex({ ...hsb, [channel]: newValue });
-    onColorChange(newHex);
+    if (inputInvalid) {
+      return;
+    }
+
+    const newHsv = { ...hsv, [channel]: newValue };
+    const newHex = hsvToHex(newHsv);
+
+    setHsv(newHsv);
+    setCachedHex(newHex);
+    onHexChange(newHex);
   };
 
+  // The "Ugliest Pattern in React", courtesy of the official docs themselves.
+  // It's basically useEffect, except that the re-render has better performance
+  // because only this component re-renders; the child components aren't allowed
+  // to re-render until this parent has a stable render without state changes
+  const needStateSync = hexColor !== cachedHex;
+  if (needStateSync) {
+    const freshHsv = rgbToHsv(rgb);
+    setHsv(freshHsv);
+    setCachedHex(hexColor);
+  }
+
   return (
-    <section>
+    <fieldset className="w-full">
       <HueWheel
-        hue={hsb.hue}
-        onHueChange={(newValue) => onHsbChannelChange("hue", newValue)}
+        hue={hsv.hue}
+        onHueChange={(newValue) => onHsvChannelChange("hue", newValue)}
       />
 
-      <SaturationBrightnessInputs
-        hsb={hsb}
-        onChannelChange={onHsbChannelChange}
-      />
-
+      <ValueSaturationInputs hsv={hsv} onChannelChange={onHsvChannelChange} />
       <RgbInputs rgb={rgb} onChannelChange={onRgbChannelChange} />
-    </section>
-  );
-}
-
-export default function ColorPickerTester() {
-  const [hex, setHex] = useState("#ffff00");
-
-  return (
-    <div
-      style={{
-        width: "500px",
-        height: "500px",
-        backgroundColor: hex,
-      }}
-    >
-      <ColorPicker hexColor={hex} onColorChange={(newHex) => setHex(newHex)} />
-    </div>
+    </fieldset>
   );
 }
