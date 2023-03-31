95a6fa48ae460d44723ba272869efa020e5523f0
Parkreiner <throwawayclover@gmail.com>
2023-03-31 16:09:33 -0400

    debug: Add debug square for each color channel

--- src/components/CharacterPreview/CharacterPreview.tsx
+++ src/components/CharacterPreview/CharacterPreview.tsx
@@ -1,27 +1,55 @@
 import { Character } from "@/typesConstants/gameData";
 import { CharacterColors } from "@/typesConstants/colors";
+import { PropsWithChildren } from "react";
 
 type Props = {
   selectedCharacter: Character | null;
   colors: CharacterColors;
 };
 
+type DebugSquareProps = PropsWithChildren<{
+  color: string;
+}>;
+
+function DebugSquare({ color, children }: DebugSquareProps) {
+  return (
+    <div
+      className="flex h-[100px] w-[100px] items-center justify-center"
+      style={{ backgroundColor: color }}
+    >
+      <div>{children}</div>
+    </div>
+  );
+}
+
 export default function CharacterPreview({ selectedCharacter, colors }: Props) {
   const characterClass = selectedCharacter?.class ?? "Unknown";
   const id = selectedCharacter?.id ?? "Unknown";
 
   return (
-    <div className="min-w-[500px] flex-grow self-stretch bg-white">
+    <div className="min-w-[500px] flex-grow self-stretch rounded-md bg-white p-6">
       <p>
         Class: {characterClass}
         <br />
         ID: {id}
       </p>
 
-      <div
-        className="h-[200px] w-[200px]"
-        style={{ backgroundColor: colors.skin[0] }}
-      />
+      <div className="flex flex-row flex-wrap gap-2">
+        <DebugSquare color={colors.skin[0]}>Skin 1</DebugSquare>
+        <DebugSquare color={colors.skin[1]}>Skin 2</DebugSquare>
+
+        <DebugSquare color={colors.hair[0]}>Hair 1</DebugSquare>
+        <DebugSquare color={colors.hair[1]}>Hair 2</DebugSquare>
+
+        <DebugSquare color={colors.eyes[0]}>Eyes 1</DebugSquare>
+        <DebugSquare color={colors.eyes[1]}>Eyes 2</DebugSquare>
+
+        {colors.misc.map((color, index) => (
+          <DebugSquare key={index} color={color}>
+            Misc {index + 1}
+          </DebugSquare>
+        ))}
+      </div>
     </div>
   );
 }
