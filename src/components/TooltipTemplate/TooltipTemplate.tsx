de56b1b0c90086012552e3a9b0114df4d4c5fc3a
Parkreiner <throwawayclover@gmail.com>
2023-04-02 13:03:42 -0400

    minor: Fix arrow color for TooltipTemplate

--- src/components/TooltipTemplate/TooltipTemplate.tsx
+++ src/components/TooltipTemplate/TooltipTemplate.tsx
@@ -10,7 +10,7 @@ export default function TooltipTemplate({ labelText, children }: Props) {
     <Tooltip.Root defaultOpen={false}>
       <Tooltip.Content>
         <p className="rounded-md bg-black py-2 px-4 text-white">{labelText}</p>
-        <Tooltip.Arrow />
+        <Tooltip.Arrow className="fill-black" />
       </Tooltip.Content>
 
       <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
