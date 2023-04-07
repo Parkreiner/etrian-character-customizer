2b1cb2b5a86a120e36f2433df48d10b06dcbd5fc
Parkreiner <throwawayclover@gmail.com>
2023-04-07 18:30:01 -0400

    minor: ErrorBoundary - Add fallback UI prop

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,6 +1,25 @@
 import { PropsWithChildren, Component, ErrorInfo } from "react";
 
-export default class ErrorBoundary extends Component<PropsWithChildren> {
+type Props = PropsWithChildren<{
+  fallback?: JSX.Element;
+}>;
+
+function DefaultFallback() {
+  return (
+    <section className="flex h-full w-full flex-col items-center justify-center">
+      <div className="max-w-2xl rounded-xl bg-white p-12 shadow-lg">
+        <h2 className="mb-4 text-2xl font-bold">Error</h2>
+        <p>
+          The app has run into a breaking error. The error has been logged, and
+          a fix will be in the works soon. Please try refreshing the page, but
+          really sorry about the inconvenience.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default class ErrorBoundary extends Component<Props> {
   state = { errored: false };
 
   static getDerivedStateFromError() {
@@ -12,24 +31,13 @@ export default class ErrorBoundary extends Component<PropsWithChildren> {
   }
 
   render() {
-    const { children } = this.props;
+    const { children, fallback } = this.props;
     const { errored } = this.state;
 
     if (!errored) {
       return <>{children}</>;
     }
 
-    return (
-      <section className="flex h-full w-full flex-col items-center justify-center">
-        <div className="max-w-2xl rounded-xl bg-white p-12 shadow-lg">
-          <h2 className="mb-4 text-2xl font-bold">Error</h2>
-          <p>
-            The app has run into a breaking error. The error has been logged,
-            and a fix will be in the works soon. Please try refreshing the page,
-            but really sorry about the inconvenience.
-          </p>
-        </div>
-      </section>
-    );
+    return fallback ?? <DefaultFallback />;
   }
 }
