c336dabd0ac6cf018079fac220b1d56dbfbd931a
Parkreiner <throwawayclover@gmail.com>
2023-04-08 16:20:39 -0400

    minor: Tabs - Improve performance of wrappers

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -9,10 +9,19 @@
  * immediately export them with a type assertion applied, because there'd be
  * nowhere to put the type parameter.
  *
+ * The wrapper components make extensive use of React.createElement instead of
+ * JSX syntax. Even though the wrappers are needed for TypeScript, they should
+ * still be as lightweight as possible. All forms of JSX would require
+ * destructuring and cloning the props objects for every single render. But the
+ * wrappers can safely for
+ * these wrappers, they can safely let their  safely be passed along with no changes. Sadly, the
+ * only way to do that is by wiring things up manually.
+ *
  * @todo Still need to figure out how to pass a type parameter from a parent
  * Tabs component to a child Tabs component. From the docs, Root leads to either
  * List or Content, and List leads to Trigger
  */
+import { createElement } from "react";
 import * as RadixTabs from "@radix-ui/react-tabs";
 import type {
   TabsProps as RadixTabsProps,
@@ -39,37 +48,26 @@ export type ContentProps<T extends string> = PatchProps<T, RadixContentProps>;
 export function Content<T extends string>(
   props: ContentProps<T>
 ): ReturnType<typeof RadixTabs.Content> {
-  return <RadixTabs.Content {...props} />;
+  return createElement(RadixTabs.Content, props);
 }
 
 export function List<T extends string>(
   props: ListProps<T>
 ): ReturnType<typeof RadixTabs.List> {
-  return <RadixTabs.List {...props} />;
+  return createElement(RadixTabs.List, props);
 }
 
-export function Trigger<T extends string>({
-  ...props
-}: TriggerProps<T>): ReturnType<typeof RadixTabs.Trigger> {
-  return <RadixTabs.Trigger {...props} />;
+export function Trigger<T extends string>(
+  props: TriggerProps<T>
+): ReturnType<typeof RadixTabs.Trigger> {
+  return createElement(RadixTabs.Trigger, props);
 }
 
-type RootReturn<T extends string> = ReturnType<
-  React.ForwardRefExoticComponent<
-    TabsProps<T> & React.RefAttributes<HTMLDivElement>
-  >
->;
-
-export const Root = <T extends string>({
-  onValueChange,
-  ...delegated
-}: TabsProps<T>): RootReturn<T> => {
-  type RadixOnValueChange = RadixTabsProps["onValueChange"];
-
-  return (
-    <RadixTabs.Root
-      {...delegated}
-      onValueChange={onValueChange as RadixOnValueChange}
-    />
+export function Root<T extends string>(
+  props: TabsProps<T>
+): ReturnType<typeof RadixTabs.Root> {
+  return createElement(
+    RadixTabs.Root,
+    props as typeof props & { onValueChange: RadixTabsProps["onValueChange"] }
   );
-};
+}
