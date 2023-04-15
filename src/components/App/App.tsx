cc4c2fb2364513455713ecaf61803379be1cd3c9
Parkreiner <throwawayclover@gmail.com>
2023-04-15 14:53:17 -0400

    refactor: App - Clean up implementation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,65 +10,58 @@ import LoadingIndicator from "@/components/LoadingIndicator";
 import PortraitMenus from "@/components/CharacterMenus";
 import ColorMenus from "@/components/ColorMenus";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import { useState } from "react";
 
-type MainProps = Omit<
-  ReturnType<typeof useAppState> & { initialized: true },
-  "initialized"
->;
-
-function MainContent({
-  selectedCharacter,
-  colors,
-  groupedCharacters,
-  stateUpdaters,
-}: MainProps) {
-  const characterId = selectedCharacter?.id ?? "";
+function Main() {
+  const [introAnimationFinished, setIntroAnimationFinished] = useState(false);
+  const appState = useAppState();
+  const characterId = appState.selectedCharacter?.id ?? "";
 
   return (
     <div className="font-cool mx-auto flex h-full max-w-[1400px] items-center justify-center">
       <div className="flex max-h-[800px] flex-row items-center justify-center gap-x-10">
-        <PortraitMenus
-          selectedCharacterId={characterId}
-          groupedCharacters={groupedCharacters}
-          onCharacterChange={stateUpdaters.changeCharacter}
-        />
+        <VisuallyHidden>
+          <h1>Etrian Odyssey Character Customizer</h1>
+        </VisuallyHidden>
 
-        <CharacterPreview
-          selectedCharacter={selectedCharacter}
-          colors={colors}
-        />
+        {!introAnimationFinished && (
+          <LoadingIndicator
+            appLoaded={appState.initialized}
+            onAnimationCompletion={() => setIntroAnimationFinished(true)}
+          />
+        )}
 
-        <ColorMenus
-          characterKey={characterId}
-          colors={colors}
-          onColorChange={stateUpdaters.replaceColors}
-        />
+        {appState.initialized && (
+          <>
+            <PortraitMenus
+              selectedCharacterId={characterId}
+              groupedCharacters={appState.groupedCharacters}
+              onCharacterChange={appState.stateUpdaters.changeCharacter}
+            />
+
+            <CharacterPreview
+              selectedCharacter={appState.selectedCharacter}
+              colors={appState.colors}
+            />
+
+            <ColorMenus
+              characterKey={characterId}
+              colors={appState.colors}
+              onColorChange={appState.stateUpdaters.replaceColors}
+            />
+          </>
+        )}
       </div>
     </div>
   );
 }
 
 export default function App() {
-  const appState = useAppState();
-
   return (
     <div className="h-full w-full">
       <ErrorBoundary>
         <TooltipProvider delayDuration={500}>
-          <VisuallyHidden>
-            <h1>Etrian Character Customizer</h1>
-          </VisuallyHidden>
-
-          {appState.initialized ? (
-            <MainContent
-              colors={appState.colors}
-              groupedCharacters={appState.groupedCharacters}
-              selectedCharacter={appState.selectedCharacter}
-              stateUpdaters={appState.stateUpdaters}
-            />
-          ) : (
-            <LoadingIndicator />
-          )}
+          <Main />
         </TooltipProvider>
       </ErrorBoundary>
     </div>
