3e42b1fee7010b81fc73f11826477ba05c79f650
Parkreiner <throwawayclover@gmail.com>
2023-04-19 10:08:13 -0400

    minor: Add rest of mock colors for Protectors

--- src/components/Editor/editorStateMocks.ts
+++ src/components/Editor/editorStateMocks.ts
@@ -1,79 +1,114 @@
-import { Character } from "@/typesConstants/gameData";
-import { ApiResponse } from "./localTypes";
+import { Character, ClassOrderings } from "@/typesConstants/gameData";
+import { ApiResponse } from "@/hooks/useGameInfoFetch";
+
+const baseCharacter: Omit<Character, "id" | "colors"> = {
+  game: "eo1",
+  class: "protector",
+  imgUrl: "http://jank.com",
+
+  svgs: {
+    leftEye: ["", ""],
+    rightEye: ["", ""],
+    hair: ["", ""],
+    skin: ["", ""],
+    misc: [],
+  },
+};
+
+const colorsForProtectors: readonly Character["colors"][] = [
+  {
+    skin: ["#f4dac5", "#cb988f"],
+    hair: ["#da7c48", "#814246"],
+    leftEye: ["#b17850", "#87494a"],
+    rightEye: ["#b17850", "#87494a"],
+    misc: [],
+  },
+  {
+    skin: ["#fcf0d7", "#c08c7c"],
+    hair: ["#f8d79a", "#f8d79a"],
+    leftEye: ["#b4b0e3", "#4e4eaa"],
+    rightEye: ["#b4b0e3", "#4e4eaa"],
+    misc: [],
+  },
+  {
+    skin: ["#fffde4", "#cf9280"],
+    hair: ["#fae4a3", "#f8d79a"],
+    leftEye: ["#b5b6ee", "#5a57bd"],
+    rightEye: ["#b5b6ee", "#5a57bd"],
+    misc: [],
+  },
+  {
+    skin: ["#fffde2", "#cb917f"],
+    hair: ["#f1a172", "#b5534e"],
+    leftEye: ["#edcdab", "#a88b64"],
+    rightEye: ["#edcdab", "#a88b64"],
+    misc: [],
+  },
+  {
+    skin: ["#d3a697", "#77524b"],
+    hair: ["#f19a96", "#a95f57"],
+    leftEye: ["#bbc5b2", "#676b59"],
+    rightEye: ["#bbc5b2", "#676b59"],
+    misc: [],
+  },
+];
+
+const classOrderings: ClassOrderings = {
+  eo1: [
+    "landsknecht",
+    "survivalist",
+    "protector",
+    "dark hunter",
+    "medic",
+    "alchemist",
+    "troubadour",
+    "ronin",
+    "hexer",
+  ],
+
+  eo2: [
+    "landsknecht",
+    "survivalist",
+    "protector",
+    "dark hunter",
+    "medic",
+    "alchemist",
+    "troubadour",
+    "ronin",
+    "hexer",
+    "gunner",
+    "war magus",
+    "beast",
+  ],
+
+  eo3: [
+    "sovereign",
+    "gladiator",
+    "hoplite",
+    "buccaneer",
+    "ninja",
+    "monk",
+    "zodiac",
+    "wildling",
+    "arbalist",
+    "farmer",
+    "shogun",
+    "yggdroid",
+  ],
+};
 
 export async function mockFetchCharacters(url: string) {
   console.log(`Provided URL: ${url}`);
 
-  const baseCharacter: Omit<Character, "id"> = {
-    game: "eo1",
-    class: "protector",
-    imgUrl: "http://jank.com",
-
-    colors: {
-      skin: ["#f4dac5", "#cb988f"],
-      hair: ["#da7c48", "#814246"],
-      leftEye: ["#b17850", "#87494a"],
-      rightEye: ["#b17850", "#87494a"],
-      misc: [],
-    },
-
-    svgs: {
-      leftEye: ["", ""],
-      rightEye: ["", ""],
-      hair: ["", ""],
-      skin: ["", ""],
-      misc: [],
-    },
-  };
-
   const res: ApiResponse = {
+    classOrderings,
     characters: [1, 2, 3, 4, 5].map((num) => {
-      return { ...baseCharacter, id: String(num) };
+      return {
+        ...baseCharacter,
+        id: String(num),
+        colors: colorsForProtectors[num - 1] as Character["colors"],
+      };
     }),
-
-    classOrderings: {
-      eo1: [
-        "landsknecht",
-        "survivalist",
-        "protector",
-        "dark hunter",
-        "medic",
-        "alchemist",
-        "troubadour",
-        "ronin",
-        "hexer",
-      ],
-
-      eo2: [
-        "landsknecht",
-        "survivalist",
-        "protector",
-        "dark hunter",
-        "medic",
-        "alchemist",
-        "troubadour",
-        "ronin",
-        "hexer",
-        "gunner",
-        "war magus",
-        "beast",
-      ],
-
-      eo3: [
-        "sovereign",
-        "gladiator",
-        "hoplite",
-        "buccaneer",
-        "ninja",
-        "monk",
-        "zodiac",
-        "wildling",
-        "arbalist",
-        "farmer",
-        "shogun",
-        "yggdroid",
-      ],
-    },
   };
 
   return new Promise<ApiResponse>((resolve) => {
