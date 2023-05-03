bd4037355d7217687f70ba81e04ec59b08235311
Parkreiner <throwawayclover@gmail.com>
2023-05-03 13:48:26 -0400

    fix: Update wrapHue helper to handle offsets larger than 1

--- src/components/ColorHueWheel/localHelpers.ts
+++ src/components/ColorHueWheel/localHelpers.ts
@@ -2,6 +2,7 @@
  * Makes sure that if a hue value would be incremented/decremented outside the
  * range of 0-359, the value wraps around to the opposite end, like a circle.
  */
-export function wrapHue(intermediateHue: number) {
-  return intermediateHue < 0 ? 359 : intermediateHue % 360;
+export function wrapHue(unwrappedHue: number) {
+  const constrained = unwrappedHue % 360;
+  return constrained < 0 ? 360 + constrained : constrained;
 }
