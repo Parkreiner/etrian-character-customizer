abc1b36144a1c6bbd167d8fce6a278d9e7446d17
Parkreiner <throwawayclover@gmail.com>
2023-06-04 13:45:32 -0400

    minor: Tweak proportions for CharacterPreview layout

--- src/components/CharacterPreview/CharacterPreview.tsx
+++ src/components/CharacterPreview/CharacterPreview.tsx
@@ -65,61 +65,63 @@ export default function CharacterPreview({ character, colors }: Props) {
     isDownloading || status === "error" || status === "loading";
 
   return (
-    <section className="flex h-full flex-grow flex-col flex-nowrap justify-center py-8">
-      <div ref={containerRef} className="w-full flex-grow py-4">
+    <section className="flex h-full flex-grow flex-col flex-nowrap justify-center gap-y-4 py-10">
+      <div ref={containerRef} className="w-full flex-shrink flex-grow py-4">
         {/*
          * Reminder: You cannot set any CSS properties on a canvas that would
          * change its size. You have to update the HTML directly (doing the
-         * math for it), or else the canvas output will distort
+         * math for it), or else the canvas output will become distorted
          */}
         <canvas ref={canvasRef} className="mx-auto">
           A preview of a {character.class} from {character.game}
         </canvas>
       </div>
 
-      <fieldset className="mx-auto flex max-w-fit flex-row flex-nowrap items-center gap-x-4 pt-6">
-        <legend>
-          <VisuallyHidden.Root>Main app button controls</VisuallyHidden.Root>
-        </legend>
+      <div>
+        <fieldset className="mx-auto flex max-w-fit flex-row flex-nowrap items-center gap-x-4">
+          <legend>
+            <VisuallyHidden.Root>Main app button controls</VisuallyHidden.Root>
+          </legend>
 
-        <GuideButton
-          buttonText="Help"
-          modalTitle="Help"
-          modalDescription="How to use this application"
-        >
-          Baba-booey Baba-booey
-        </GuideButton>
+          <GuideButton
+            buttonText="Help"
+            modalTitle="Help"
+            modalDescription="How to use this application"
+          >
+            Baba-booey Baba-booey
+          </GuideButton>
 
-        <button
-          type="button"
-          className="select-none rounded-full bg-teal-800 px-7 py-3 text-xl font-medium text-teal-50 shadow-md transition-colors"
-          disabled={downloadsDisabled}
-          onClick={downloadAllImages}
-        >
-          Download
-        </button>
+          <button
+            type="button"
+            className="select-none rounded-full bg-teal-800 px-7 py-3 text-xl font-medium text-teal-50 shadow-md transition-colors"
+            disabled={downloadsDisabled}
+            onClick={downloadAllImages}
+          >
+            Download
+          </button>
 
-        <GuideButton
-          buttonText="Credits"
-          modalTitle="Credits"
-          modalDescription="Credits and colophon"
-        >
-          Baba-booey Baba-booey
-        </GuideButton>
-      </fieldset>
+          <GuideButton
+            buttonText="Credits"
+            modalTitle="Credits"
+            modalDescription="Credits and colophon"
+          >
+            Baba-booey Baba-booey
+          </GuideButton>
+        </fieldset>
 
-      <p className="mx-auto mt-4 w-fit rounded-full bg-yellow-100 px-4 py-2 text-center text-sm font-medium text-yellow-900">
-        Work in progress.{" "}
-        <a
-          className="underline"
-          href="https://github.com/Parkreiner/etrian-character-customizer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check the GitHub page
-        </a>{" "}
-        for updates.
-      </p>
+        <p className="mx-auto mt-4 w-fit rounded-full bg-yellow-100 px-4 py-2 text-center text-sm font-medium text-yellow-900">
+          Work in progress.{" "}
+          <a
+            className="underline"
+            href="https://github.com/Parkreiner/etrian-character-customizer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Check the GitHub page
+          </a>{" "}
+          for updates.
+        </p>
+      </div>
     </section>
   );
 }
