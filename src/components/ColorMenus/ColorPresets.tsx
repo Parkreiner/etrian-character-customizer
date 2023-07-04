15c87ab442367c1623e62acdd56354bb1b10afa7
Parkreiner <throwawayclover@gmail.com>
2023-07-04 01:04:22 -0400

    refactor: Split off usePresetsKeyboardNav into separate file

--- src/components/ColorMenus/ColorPresets.tsx
+++ src/components/ColorMenus/ColorPresets.tsx
@@ -1,169 +1,23 @@
-/**
- * @todo This file is getting big and involved enough that it probably makes
- * sense to split it off into a separate top-level directory once I'm done
- */
-import { useCallback, useEffect, useRef, useState } from "react";
-
 import {
   ColorTuple,
   HAIR_EYE_COLOR_PRESETS,
   SKIN_COLOR_PRESETS,
   MISC_COLOR_PRESETS,
 } from "@/typesConstants/colors";
-import { ArrowKey, isArrowKey } from "@/utils/keyboard";
-import { clamp } from "@/utils/math";
 
 import Card from "@/components/Card/";
 import ColorButton from "./ColorButton";
 import { UiTab } from "./localTypes";
+import usePresetsKeyboardNav from "./usePresetsKeyboardNav";
 
-type Props = {
+type ExternalProps = {
   activeTab: UiTab;
   onHexPresetChange: (hex1: string, hex2: string) => void;
 };
 
-export function mapKeyToGridIndex(
-  itemCount: number,
-  gridCount: number,
-  currentIndex: number,
-  arrowKey: ArrowKey
-): number {
-  const rowIndex = Math.floor(currentIndex / gridCount);
-  const colIndex = currentIndex % gridCount;
-  const lastRowIndex = Math.floor(itemCount / gridCount);
-  const lastColIndex = Math.floor(itemCount / lastRowIndex);
-
-  let offsetRowIndex = rowIndex;
-  let offsetColIndex = colIndex;
-  switch (arrowKey) {
-    // Only ArrowLeft seems to be 100% working right now; all other arrows have
-    // issues if you're dealing with a grid that has blanks
-    case "ArrowLeft": {
-      offsetColIndex = colIndex === 0 ? gridCount - 1 : colIndex - 1;
-      break;
-    }
-
-    case "ArrowRight": {
-      offsetColIndex = colIndex === gridCount - 1 ? 0 : colIndex + 1;
-      break;
-    }
-
-    case "ArrowUp": {
-      offsetRowIndex = rowIndex === 0 ? lastRowIndex : rowIndex - 1;
-      break;
-    }
-
-    case "ArrowDown": {
-      offsetRowIndex = rowIndex === lastRowIndex ? 0 : rowIndex + 1;
-      break;
-    }
-
-    default: {
-      throw new Error("Unknown input received.");
-    }
-  }
-
-  const lastItemIndex = itemCount - 1;
-  const computedIndex = lastColIndex * offsetRowIndex + offsetColIndex;
-  return Math.min(computedIndex, lastItemIndex);
-}
-
-function usePresetsKeyboardNav<Element extends HTMLElement>(
-  numPresets: number
-) {
-  const [activePresetIndex, setActivePresetIndex] = useState(0);
-
-  // Pretty sure I can get away with just using a ref value for the column
-  // count, since the value will only ever be used within effects
-  const gridColumnCountRef = useRef(0);
-  const gridContainerRef = useRef<Element>(null);
-  const updateInfoRef = useRef({ activePresetIndex, numPresets });
-
-  // Effect for updating the number of columns as the element resizes
-  useEffect(() => {
-    const gridContainer = gridContainerRef.current;
-    if (gridContainer === null) return;
-
-    const updateColumnCountOnResize = () => {
-      const gridStyling = window.getComputedStyle(gridContainer);
-      const count = gridStyling
-        .getPropertyValue("grid-template-columns")
-        .split(" ").length;
-
-      gridColumnCountRef.current = count;
-    };
-
-    const observer = new ResizeObserver(updateColumnCountOnResize);
-    observer.observe(gridContainer);
-    return () => observer.disconnect();
-  }, []);
-
-  useEffect(() => {
-    updateInfoRef.current = { activePresetIndex, numPresets };
-  }, [numPresets, activePresetIndex]);
-
-  // Effect for listening to keyboard input; must run after column resize effect
-  useEffect(() => {
-    const gridContainer = gridContainerRef.current;
-    if (gridContainer === null) return;
-
-    const handleKeyInput = (event: KeyboardEvent) => {
-      const { key } = event;
-      if (!isArrowKey(key)) return;
-
-      event.preventDefault();
-      const { activePresetIndex, numPresets } = updateInfoRef.current;
-
-      if (numPresets === 0) {
-        return;
-      }
-
-      const newIndex = mapKeyToGridIndex(
-        numPresets,
-        gridColumnCountRef.current,
-        activePresetIndex,
-        key
-      );
-
-      setActivePresetIndex(newIndex);
-    };
-
-    gridContainer.addEventListener("keydown", handleKeyInput);
-    return () => gridContainer.removeEventListener("keydown", handleKeyInput);
-  }, []);
-
-  /**
-   * @todo Effect is only for debugging - delete once fully done (and tested!)
-   */
-  useEffect(() => {
-    const gridContainer = gridContainerRef.current;
-    if (gridContainer === null) return;
-
-    const intervalId = window.setInterval(() => {
-      gridContainer.dispatchEvent(
-        new KeyboardEvent("keydown", { key: "ArrowRight" })
-      );
-    }, 500);
-
-    return () => window.clearInterval(intervalId);
-  }, []);
-
-  const safeSetActivePresetIndex = useCallback(
-    (newIndex: number) => {
-      setActivePresetIndex((current) => {
-        if (!Number.isInteger(newIndex) || newIndex < 0) return current;
-        return clamp(newIndex, 0, numPresets - 1);
-      });
-    },
-    [numPresets]
-  );
-
-  return {
-    gridContainerRef,
-    activePresetIndex,
-    setActivePresetIndex: safeSetActivePresetIndex,
-  } as const;
-}
+type CoreProps = Omit<ExternalProps, "activeTab"> & {
+  colorTuples: readonly ColorTuple[];
+};
 
 const colorPresets = {
   eyes: HAIR_EYE_COLOR_PRESETS,
@@ -172,17 +26,7 @@ const colorPresets = {
   misc: MISC_COLOR_PRESETS,
 } as const satisfies Record<UiTab, readonly ColorTuple[]>;
 
-type PresetsListProps = Omit<Props, "activeTab"> & {
-  colorTuples: readonly ColorTuple[];
-};
-
-/**
- * @todo Make empty state for when colors aren't available
- * @todo Extract out the arrow key logic into a separate utils file
- * @todo Also need to figure out how tab indexing and keyboard navigation even
- * work here.
- */
-function ColorPresetList({ colorTuples, onHexPresetChange }: PresetsListProps) {
+function ColorPresetsCore({ colorTuples, onHexPresetChange }: CoreProps) {
   const { gridContainerRef, activePresetIndex, setActivePresetIndex } =
     usePresetsKeyboardNav<HTMLUListElement>(colorTuples.length);
 
@@ -213,13 +57,16 @@ function ColorPresetList({ colorTuples, onHexPresetChange }: PresetsListProps) {
   );
 }
 
-export default function ColorPresets({ activeTab, onHexPresetChange }: Props) {
+export default function ColorPresets({
+  activeTab,
+  onHexPresetChange,
+}: ExternalProps) {
   const selectedList = colorPresets[activeTab];
 
   return (
     <Card title={`Presets (${activeTab})`} striped gapSize="small">
       {selectedList.length > 0 ? (
-        <ColorPresetList
+        <ColorPresetsCore
           key={activeTab}
           colorTuples={selectedList}
           onHexPresetChange={onHexPresetChange}
